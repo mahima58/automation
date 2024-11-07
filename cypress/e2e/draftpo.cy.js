82c83b54182c2f51332012f9d0993c82e87cdecc
Mahima58 <mahima@soorisolutions.com.np>
2024-11-07 10:03:13 +0545

    draft po

---
+++ cypress/e2e/draftpo.cy.js
@@ -0,0 +1,49 @@
+describe('Create Purchase Order after Login', () => {
+    beforeEach("load fixture", function () {
+      // Load fixture and assign alias
+      cy.fixture("item").as('itemData');
+      cy.visit('https://staging.soorisolutions.com.np/purchase-order');
+    });
+  
+    it('Create PO', function () {
+      // Login process
+      cy.wait(3000);
+      cy.get('#UserName').type('mahima');
+      cy.get('#Password').type('@mahima123');
+      cy.get('.btn').click();
+      cy.wait(3000);
+      // Ensure login was successful
+      cy.url().should('be.equal', 'https://staging.soorisolutions.com.np/purchase-order');
+      cy.get('.row > :nth-child(2) > .button-container > .btn').click();
+      cy.get('.css-kpcqxn-control > .css-1wy0on6 > .css-tlfecz-indicatorContainer').click();
+      cy.wait(2000);
+      cy.get('#react-select-12-option-0').click();
+      cy.get(':nth-child(2) > .col-10 > .my-2 > .common-async-select-wrapper > .async-select-field > .css-z3azwa-control > .css-1wy0on6 > .css-tlfecz-indicatorContainer').click();
+      cy.wait(2000);
+      cy.get('#react-select-13-option-0').click();
+      cy.get(':nth-child(4) > .col-10 > .my-2 > .common-async-select-wrapper > .async-select-field > .css-z3azwa-control > .css-1wy0on6 > .css-tlfecz-indicatorContainer').click();
+      cy.wait(2000);
+      cy.get('#react-select-15-option-0').click();
+      cy.get(':nth-child(5) > .col-10 > .my-2 > .common-async-select-wrapper > .async-select-field > .css-z3azwa-control > .css-1wy0on6 > .css-tlfecz-indicatorContainer').click();
+      cy.wait(2000);
+      cy.get('#react-select-16-option-0').click();
+      
+      // Use the fixture data loaded via 'cy.get' with alias
+      cy.get('@itemData').then((data) => {  // Access fixture data using 'data'
+        for (let i = 0; i < 5; i++) {
+          cy.get('.purchase-order-form-container > .col-3 > .col-10 > .my-2 > .common-async-select-wrapper > .async-select-field > .css-z3azwa-control > .css-1wy0on6 > .css-tlfecz-indicatorContainer').click();
+          cy.get('.css-11unzgr').type(data.item[i]);
+          cy.wait(5000);
+          cy.get('.css-11unzgr').contains(data.item[i]).click();
+          cy.get(':nth-child(2) > .my-2 > .common-async-select-wrapper > .async-select-field > .css-z3azwa-control > .css-1wy0on6 > .css-tlfecz-indicatorContainer').click();
+          cy.get('.css-11unzgr').contains('Unit').click();         
+          cy.get('.form-control.undefined').eq(6).type('5');  // Quantity
+          cy.get('.form-control.undefined').eq(8).type('500');  // Price
+          cy.get('[style="margin: 1.9rem 0rem;"] > .button-container > .btn').click(); //add button
+        }
+        cy.get('.btn-close').click();    //cancel button
+        cy.get('.btn.btn-secondary.btn-sm').eq(1).click(); //save draft
+      });
+    });
+  });
+  
